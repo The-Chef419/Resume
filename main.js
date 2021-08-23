67d02330dd10d72c91cd33c17fe687842a3fba3b
The-Chef419 <kontactemi@gmail.com>
2021-08-23 17:55:42 +0100

    9

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ let opt = {
     filename: 'CV.pdf',
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 4 },
-    jsPDF: { format: 'a4', orientation: 'portrait' }
+    jsPDF: { format: 'a2', orientation: 'portrait' }
 };
 
 
