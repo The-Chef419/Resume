4de5613df11941bca1ea47d70c1517cf8067ca45
The-Chef419 <kontactemi@gmail.com>
2021-08-23 16:26:36 +0100

    final updates

--- main.js
+++ main.js
@@ -16,12 +16,13 @@ const navLink = document.querySelectorAll('.nav_link')
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu')
+
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/* SCROLL SECTIONS ACTIVE LINK */
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
@@ -33,16 +34,16 @@ function scrollActive() {
         sectionId = current.getAttribute('id')
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelectorAll('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
         } else {
-            document.querySelectorAll('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
 
 
-/*==================== SHOW SCROLL TOP ====================*/
+/* SHOW SCROLL TOP */
 function scrollTop() {
     const scrollTop = document.getElementById('scroll-top');
 
@@ -52,7 +53,7 @@ function scrollTop() {
 
 window.addEventListener('scroll', scrollTop)
 
-/*==================== DARK LIGHT THEME ====================*/
+/* DARK LIGHT THEME */
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
@@ -64,47 +65,43 @@ const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dar
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon ' : 'bx-sun'
 
 if (selectedTheme) {
+
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
+
 themeButton.addEventListener('click', () => {
+
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
 
     localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon)
+    localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
+/* REDUCE THE SIZE AND PRINT ON AN A4 SHEET */
 function scaleCv() {
     document.body.classList.add('scale-cv')
 }
 
-/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
+/* REMOVE THE SIZE WHEN THE CV IS DOWNLOADED */
 function removeScale() {
     document.body.classList.remove('scale-cv')
 }
 
-/*==================== GENERATE PDF ====================*/
+/* GENERATE PDF */
 // PDF generated area
-let areaCv = document.getElementById('area-cv')
+let areaCV = document.getElementById('CV')
 
 let resumeButton = document.getElementById('resume-button')
 
-// Html2pdf options
-let opt = {
-    margin: 0,
-    filename: 'resume.pdf',
-    image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 4 },
-    jsPDF: { format: 'a4', orientation: 'portrait' }
-};
 
-// Function to call areaCv and Html2Pdf options 
+// Function to call areaCV and Html2Pdf options 
 function generateResume() {
-    html2pdf(areaCv, opt)
+    html2pdf(areaCV)
 }
+
 
 // When the button is clicked, it executes the three functions
 resumeButton.addEventListener('click', () => {
@@ -112,7 +109,6 @@ resumeButton.addEventListener('click', () => {
     scaleCv()
 
 
-    generateResume
 
-    setTimeout(removeScale, 5000)
+
 })
